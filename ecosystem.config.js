203502f3de43ce804e11183fbf80e5f27072dd12
Ben Rabidou <ben.rabidou@gmail.com>
2018-11-29 14:01:48 -0500

    Hide the key

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -23,7 +23,7 @@ module.exports = {
    deploy : {
      // "production" is the environment name
      production : {
-       key  : "/Users/benrabidou/Dropbox/Keys/benrabidou.pem",
+       //key  : "/Users/benrabidou/Dropbox/Keys/benrabidou.pem",
        user : "ubuntu",
        host : ["slackops.rabidou.io"],
        ref  : "origin/master",
