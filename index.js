aff99093033e0da86607a94fe00f4d3dbd7755c0
Ben Rabidou <ben.rabidou@gmail.com>
2018-12-02 13:51:17 -0500

    update to try and fix the server

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const SlackOps          = require('./SlackOps/app');
 const LogHelper         = require('./SlackOps/utilities/log_helper');
+const SlackOps          = require('./SlackOps/app');
 const config            = require('./config');
 const express           = require('express');
 const app               = express();
@@ -7,6 +7,8 @@ const app               = express();
 const env               = process.env.NODE_ENV || config.server.node_env;
 const port              = process.env.PORT     || config.server.port;
 const env_confg         = config[env] || {};
+
+LogHelper.log('Light em up up up....This apps on FIRE!');
 
 app.use('/', SlackOps);
 app.listen(port, () => LogHelper.log('"' + env + '" Server is running at port : ' + port))
